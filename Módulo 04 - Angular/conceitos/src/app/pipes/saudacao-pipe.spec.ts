879936db7160ad8d32757ecf7691626fa49a372c
Maria Clara <58827966+Wazotti@users.noreply.github.com>
2025-08-26 14:07:36 -0300

    Delete Módulo 04 - Angular directory

--- Módulo 04 - Angular/conceitos/src/app/pipes/saudacao-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SaudacaoPipe } from './saudacao-pipe';
-
-describe('SaudacaoPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SaudacaoPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
