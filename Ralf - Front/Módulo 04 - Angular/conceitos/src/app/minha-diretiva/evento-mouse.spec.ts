72685da8237e6c482df0d6ea55ac9b044506c32a
Maria Clara <58827966+Wazotti@users.noreply.github.com>
2025-08-29 18:20:22 -0300

    Delete Ralf - Front/Módulo 04 - Angular/conceitos directory

--- Ralf - Front/Módulo 04 - Angular/conceitos/src/app/minha-diretiva/evento-mouse.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EventoMouse } from './evento-mouse';
-
-describe('EventoMouse', () => {
-  it('should create an instance', () => {
-    const directive = new EventoMouse();
-    expect(directive).toBeTruthy();
-  });
-});
